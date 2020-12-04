e34b3f8d9f78b0fb7ceb1067e50c49d41bd8201a
Matt Stauffer <matt@tighten.co>
2020-12-03 23:34:05 -0500

    Fix MAS

--- support/mas_apps.sh
+++ support/mas_apps.sh
@@ -1,32 +1,30 @@
-declare -A MAS_APPS
 MAS_APPS=(
-    ["amphetamine"]="937984704"
-    ["breaktime"]="427475982"
-    ["daisydisk"]="411643860"
-    ["day one"]="1055511498"
-    ["1password"]="1333432190"
-    ["Kindle"]="405399194"
-    ["MindNode"]="1289197285"
-    ["StuffIt Expander"]="919269455"
-    ["Transmit"]="1436522307"
-    ["BusyCal"]="567245998"
-    ["Tweetbot"]="1384080005"
-    ["GIPHY Capture"]="668208984"
-    ["Trello"]="1278508951"
-    ["Things"]="904280696"
-    ["Bear"]="1091189122"
-    ["Screenflow"]="1475796517"
-    ["Deliveries"]="924726344"
-    ["Soulver"]="413965349"
-    ["Divvy"]="413857545"
-    ["The Unarchiver"]="425424353"
-    ["CloudApp"]="417602904"
-    ["Paw"]="584653203"
-    ["GIF Brewery 3"]="1081413713"
-    ["Frank Deloupe"]="530458789"
+    "Amphetamine::937984704"
+    "Breaktime::427475982"
+    "Daisydisk::411643860"
+    "Day_one::1055511498"
+    "Kindle::405399194"
+    "MindNode::1289197285"
+    "StuffIt_Expander::919269455"
+    "Transmit::1436522307"
+    "BusyCal::567245998"
+    "Tweetbot::1384080005"
+    "GIPHY_Capture::668208984"
+    "Trello::1278508951"
+    "Things::904280696"
+    "Bear::1091189122"
+    "Deliveries::924726344"
+    "Soulver::413965349"
+    "Divvy::413857545"
+    "The_Unarchiver::425424353"
+    "CloudApp::417602904"
+    "Paw::584653203"
+    "GIF_Brewery_3::1081413713"
 )
 
-# @todo: Wasn't there a way to pass this like a Brewfile??
-for app in $MAS_APPS; do
-    mas install $app
+for index in "${MAS_APPS[@]}" ; do
+    KEY="${index%%::*}"
+    VALUE="${index##*::}"
+    echo "Installing: $KEY"
+    mas install $VALUE
 done
