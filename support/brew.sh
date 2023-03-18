31da81079818d6baf314ea02b418ffd4e3304436
Matt Stauffer <matt@tighten.co>
2023-03-17 22:58:37 -0400

    Mush the brewfile together

--- support/brew.sh
+++ support/brew.sh
@@ -9,27 +9,9 @@ else
 fi
 
 echo ''
-echo 'Installing brew-mas'
-echo '-------------------'
-arch -x86_64 brew install mas
-
-echo ''
-echo 'Installing Cask apps'
-echo '--------------------'
-source "$support_dir/cask_apps.sh"
-
-echo ''
-echo 'Installing Mac App Store Software'
-echo '---------------------------------'
-source "$support_dir/mas_apps.sh"
-
-echo ''
-echo 'Installing normal Homebrew dependencies'
-echo '---------------------------------------'
+echo 'Installing Homebrew dependencies'
+echo '--------------------------------'
 cd $support_dir
 arch -x86_64 brew bundle
-
-# @todo Once I run this once, try dumping to a Brewfile to see if it actually dumps
-# MAS and Cask correctly; if so, potentially replace all/most of this with just Brewfile
 
 echo 'XX -- Homebrew done.'
--- support/mac_settings.sh
+++ support/mac_settings.sh
@@ -4,14 +4,28 @@
 defaults write com.apple.finder CreateDesktop -bool false && killall Finder
 
 # choosy as browser
+echo "TODO: set choosy as browser"
+
 # disable spotlight suggestions
+echo "TODO: disable spotlight suggestions"
+
 # full keyboard access; all controls
+echo "TODO: full keyboard access; all controls"
+
 # iterm use solarized dark
+echo "TODO: iterm2 use solarized dark"
+
 # iterm use dejavu sans mono for powerline
+echo "TODO: iterm2 use dejavu sans mono for powerline"
+
 # set caps lock as esc
-# add all twitter accounts to tweetbot
+echo "TODO: set caps lock as esc"
+
 # set alfred up to sync from dropbox file
+echo "TODO: set alfred up to sync from dropbox file"
+
 # activate alfred powerpack
+echo "TODO: activate alfred powerpack"
 
 # install vundle or whatever other vim bundler
 vim +PluginInstall +qall
